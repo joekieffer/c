e28052683eff9e513003ed3e0537ed9275cbd3aa
joekieffer <joseph.kieffer@gmail.com>
2014-10-16 15:37:02 -0500

    hello and name examples with compile instructions

---
+++ hello.cpp
@@ -0,0 +1,22 @@
+/******
+** This is a shell for a program that will print hello world once compliled
+**
+**
+**/
+
+/*  comments in C++ can be done many different ways the two most common ways are
+**  // the double slashes will comment out anything to the right of them
+**  for multiple lines of text you can use the / * and * / without spaces inbetween. 
+*/
+
+
+#include<iostream> //why do we need this line?
+
+int main()        //Why do we declare the main function as int?
+{
+
+std::cout << "Output";   // dont forget to add ; at the end of all statments
+return 0;
+}
+
+//complie this and see the output
---
+++ hello_2.cpp
@@ -0,0 +1,28 @@
+/******
+** This is a shell for a program that will print hello world once compliled
+**
+**
+**/
+
+/*  comments in C++ can be done many different ways the two most common ways are
+**  // the double slashes will comment out anything to the right of them
+**  for multiple lines of text you can use the / * and * / without spaces inbetween. 
+*/
+
+
+#include<iostream> 
+
+using std::cout;  //what do these lines do?
+using std::cin;    //why is it better then using "using namespace std"?
+
+int main()
+{
+
+cout << "Whats your name? ";
+//need code to get input from the user
+//print there name back to them
+
+return 0;
+}
+
+//complie this and see the output
